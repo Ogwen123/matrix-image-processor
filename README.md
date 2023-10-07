78a9f953498012b8e9ee4b20bc93c590075c4909
Ogwen123 <owen.d.jones@btinternet.com>
2023-10-07 16:57:46 +0100

    intial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# blurrer
+lets you manipulate images using matrices (blurring, edge detection, etc)
---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <SDL.h>
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+const int DELTA_TIME = 1000/30;
+const int HEIGHT = 600;
+const int WIDTH = 600;
+
+int main(int argc, char* args []) {
+
+    vector<vector<float>> matrix = {{1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,0,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}, {1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,1}}; // greyscale value from 0 to 1
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("Blurrer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+                break;
+            }
+        }
+        //SDL_SetRenderDrawColor(renderer, 100, 100, 180, 255); // Set the background color to purple
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                SDL_Rect rect;
+                rect.x = i*(WIDTH/n);
+                rect.y = j*(HEIGHT/m);
+                rect.w = WIDTH/n;
+                rect.h = HEIGHT/m;
+                int value = (int) round(matrix[j][i] * 255);
+                // to go from grey scale to rgb just set each value of the rgb to the greyscale * 255
+                SDL_SetRenderDrawColor(renderer, value, value, value, value);
+                SDL_RenderFillRect(renderer, &rect);
+
+            }
+        }
+        SDL_RenderPresent(renderer);
+        SDL_Delay(DELTA_TIME);
+        SDL_RenderClear(renderer);
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
